b1ed835f5a4b900213f208594df2dd937357b712
edouard-todorov-formation <edouard.todorov@laplateforme.io>
2025-09-30 14:25:37 +0200

    add_img management2

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -4,9 +4,12 @@ namespace App\Form;
 
 use App\Entity\Post;
 use App\Entity\Type;
+use Doctrine\DBAL\Types\StringType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,9 +18,24 @@ class PostType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('content')
-            ->add('title')
+            ->add('name', TextType::class, [
+                "label" => "Pseudo",
+                "attr" => [
+                    "placeholder" => "Ecrire votre pseudo"
+                ]
+            ])
+            ->add('title', TextType::class, [
+                "label" => "Titre",
+                "attr" => [
+                    "placeholder" => "Ecrire le titre"
+                ]
+            ])
+            ->add('content', TextareaType::class, [
+                "label" => "Contenu",
+                "attr" => [
+                    "placeholder" => "Ecrire votre article"
+                ]
+            ])
             ->add('type', EntityType::class, [
                 'class' => Type::class,
                 'choice_label' => 'id',
